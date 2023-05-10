3a20c19b32395c77729dca854f1f76df066751d6
JoeyPerp <111063341+OdiKro95@users.noreply.github.com>
2023-05-10 16:46:04 +0000

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,34 +1,54 @@
+#!/usr/bin/python3
+""" raddit api"""
+
+import json
 import requests
 
-def count_words(subreddit, word_list, word_count={}):
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code != 200:
-        if response.status_code == 302:
-            print("Subreddit doesn't exist")
-        return
-    
-    data = response.json()['data']['children']
-    for post in data:
-        title = post['data']['title'].lower()
-        for word in word_list:
-            if ' ' in word:
-                continue
-            if word.lower() in title:
-                word_count[word] = word_count.get(word, 0) + 1
-    
-    if len(data) == 0:
-        for word, count in sorted(word_count.items(), key=lambda x: (-x[1], x[0])):
-            print(word + ': ' + str(count))
-        return
-    
-    last_post = data[-1]['data']['name']
-    count_words(subreddit, word_list, word_count)
-    
-    new_response = requests.get(url, headers=headers, params={'after': last_post}, allow_redirects=False)
-    if new_response.status_code == 200:
-        count_words(subreddit, word_list, word_count)
-    else:
-        print("An error occurred while fetching the next page of posts")
+
+def count_words(subreddit, word_list, after="", count=[]):
+    """count all words"""
+
+    if after == "":
+        count = [0] * len(word_list)
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    request = requests.get(url,
+                           params={'after': after},
+                           allow_redirects=False,
+                           headers={'user-agent': 'bhalut'})
+
+    if request.status_code == 200:
+        data = request.json()
+
+        for topic in (data['data']['children']):
+            for word in topic['data']['title'].split():
+                for i in range(len(word_list)):
+                    if word_list[i].lower() == word.lower():
+                        count[i] += 1
+
+        after = data['data']['after']
+        if after is None:
+            save = []
+            for i in range(len(word_list)):
+                for j in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[j].lower():
+                        save.append(j)
+                        count[i] += count[j]
+
+            for i in range(len(word_list)):
+                for j in range(i, len(word_list)):
+                    if (count[j] > count[i] or
+                            (word_list[i] > word_list[j] and
+                             count[j] == count[i])):
+                        aux = count[i]
+                        count[i] = count[j]
+                        count[j] = aux
+                        aux = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = aux
+
+            for i in range(len(word_list)):
+                if (count[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i].lower(), count[i]))
+        else:
+            count_words(subreddit, word_list, after, count)
